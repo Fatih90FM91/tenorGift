05d28907118a13d6078c98ee2e961fbb0febe48f
HuntermanTeam <jonsnow9091.fs@gmail.com>
2022-01-07 02:23:15 +0100

    js and css changes some features

--- app.js
+++ app.js
@@ -4,9 +4,11 @@
     var xmlHttp = new XMLHttpRequest();
 
     var inputValue= "";
+    var inputNumber= "";
     
 
     let inputTenor =document.getElementById('input-tenor');
+    let inputTenorNumber =document.getElementById('input-tenor-number');
     let btnTenor =document.getElementById('button-tenor');
 
     xmlHttp.onreadystatechange = function()
@@ -30,43 +32,73 @@ function printData(arr){
    
 
         let newTag=$('<div class="imgTenor"></div>')
+       
+        
+  
+            for (let i = 0; i < result.length; i++) {
+            let newUrl=$(`<img  src=${result[i].media[0].nanogif.url}>`);
 
-
-for (let i = 0; i < result.length; i++) {
-    let newUrl=$(`<img  src=${result[i].media[0].nanogif.url}>`);
-
-    newTag.append(newUrl);
-    $('#tenor-gift').append(newTag);
+            newTag.append(newUrl);
+            $('#tenor-gift').append(newTag);
     
   
     
-}
+              }
+
+
 
 }
+
+
   
 
 btnTenor.addEventListener('click',function(){
  
     var apikey = "X6PEFRJQZXLS";
-    var lmt = 8;
     var search_url ;
-    var search_term="cat";
+    // var search_term="cat";
+    let newTagForWarn=$('<div class="warningFor"></div>');
+    let forWarn=$('<h2> Please Try Again!!</h2>')
 
     inputValue=inputTenor.value;
+    inputNumber=inputTenorNumber.value;
+
+    if(inputValue=='' || inputNumber==''){
+       
+         console.log("try it again");
+              newTagForWarn.append(forWarn);
+        
+            $('#warning').append(newTagForWarn);
+
+    } else if(inputValue=='' && inputNumber!=''){
+
+        console.log("try it again");
+        newTagForWarn.append(forWarn);
+  
+      $('#warning').append(newTagForWarn);
+
+    }
+
     console.log( inputValue);
-      // test search term
+    // test search term
     search_term = inputValue;
     console.log(  search_term);
-
-    search_url = "https://g.tenor.com/v1/search?q=" + search_term + "&key=" +
-    apikey + "&limit=" + lmt;
-  
     
+    search_url = "https://g.tenor.com/v1/search?q=" + search_term + "&key=" +
+    apikey + "&limit=" + inputNumber;
 
+    if(inputNumber==0 || inputNumber>0){
+
+        search_url="";
+
+    }
+    
+    
+    
     xmlHttp.open('GET',search_url ,true);
-
+    
     xmlHttp.send();
-
+    
 
 
 });
